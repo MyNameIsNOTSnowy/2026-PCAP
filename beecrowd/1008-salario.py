efd9bd86ebc0ccd8340c03ec8851f06f0b734b12
MyNameIsNOTSnowy <20261ctb0100054@estudantes.ifpr.edu.br>
2026-04-09 20:12:10 +0000

    Aula 11 - 1008: salario

---
+++ beecrowd/1008-salario.py
@@ -0,0 +1,24 @@
+'''
+Problema: beecrowd | 1008
+Data: 2026.04.09
+Estudante: Otávio Rodrigues Conrado
+'''
+# Objetivo: Ler número do funcionario, horas trabalhadas e salario por hora calcular e exibir o total a receber
+
+# --- ANÁLISE (LIAC) ---
+# Entrada: número (input), horas trabalhadas (input) e salário por hora (float)
+# Processamento: salário = horas trabalhadas * salário por hora
+# Saída: exibir no formato exato "NUMBER = "numero do funcionario"" e logo abaixo "SALARY = salário a ser recebido" com 2 casas decimais
+
+# input -> lê o valor fornecido
+N = input()
+
+# int(input()) -> lê e converte em uma única instrução
+H = int(input())
+
+# float(input()) -> lê valores monetários (podem ter casas decimais)
+S = float(input())
+
+# print mostra o que quer que seja colocado entre as "" (use f antes das "" para calculos em geral)
+print(f"NUMBER = {N}")
+print(f"SALARY = U$ {H*S}")
